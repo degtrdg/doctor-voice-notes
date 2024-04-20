55109edf878a0f34ddca5404a0355c5e9ed463ff
Kanishk Garg <kanishkg2010@gmail.com>
2024-04-20 15:02:20 -0700

    fixed the audio recording bug

--- client_expo_app/constants.js
+++ client_expo_app/constants.js
@@ -1,3 +1,3 @@
-const FAST_API_URL = "https://doctor-voice-notes-production.up.railway.app/api/upload_audio"
+const FAST_API_URL = "https://kanishkgar--doctor-voice-app-fastapi-app.modal.run"
 
 export { FAST_API_URL }
