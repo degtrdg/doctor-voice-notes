375504dc3c8052654da18f96f8a2c0e16f17a4cc
Daniel George <degtrdg@github.com>
2024-04-20 13:26:38 -0500

    finished starter for making drug extract and transcript agent

--- api/prompts.py
+++ api/prompts.py
@@ -19,6 +19,56 @@ sysprompt = '''
 You are a diarization system that has been trained on a dataset of audio recordings. You have been tasked with transcribing the audio from a doctor's voice notes. The transcription might be flawed/have errors due to the transcription system not knowing domain knowledge and general errors, so you have to use your expertise to accurately write the diarization as it should be.
 '''.strip()
 
-ANSWER_PATTERN = r"(?i)ANSWER\s*:\s*(Yes|No)"
+prompt_extract_drug = '''
+You are given the current transcript between a doctor and a patient. Your job is to detect if the doctor has decided to PRESCRIBE a drug to the patient (not just if a drug is referenced). If so you need to output the drug in the specified format which will be told later.
 
+These are example drugs that are written in a specific naming convention. You want to interpret the transcript knowing that there might be errors and interpret it like the drugs that are shown below:
+```md
+Omeprazole
+Medical Marijuana
+Acetaminophen
+Warfarin
+Metformin
+Albuterol
+Sertraline
+```
+
+The transcript might say 'war for in' but if it is in the context of prescribing, you should understand that 'Warfarin' is what was intended.
+
+This is the transcript so far:
+```md
+{transcript}
+```
+
+Output a md block below with the drug that has been prescribed. Output an empty md block if no drug has been prescribed. Don't yap.
+'''.strip()
+
+sysprompt_extract_drug = '''
+You are a nurse watching over the transcript of a doctor patient conversation. You have been tasked with detecting if a drug has been prescribed to the patient. You understand that transcripts may have errors and you have to interpret the context of the conversation to determine if a drug has been prescribed by the doctor based on the transcript you have seen so far.
+'''.strip()
+
+sysprompt_checklist = '''
+You are a nurse watching over the transcript of a doctor patient conversation. You have been tasked with detecting if a drug has been prescribed to the patient. You understand that transcripts may have errors and you have to interpret the context of the conversation to determine if a drug has been prescribed by the doctor based on the transcript you have seen so far.
+'''.strip()
+
+prompt_checklist = '''
+You are given a checklist that is required to be checked off before prescribing {drug}. You are also given the transcript of the real-time patient-doctor interaction. 
+
+The following is how the checklist is currently stored in the system currently for :
+```json
+{checklist}
+```
+
+Your job is to update the JSON object and output the new version exactly. You should only be changing the boolean 'checked' attribute based on what you are seeing in the transcript. We'll be using json.load on your output to update the system so be careful to be precise in your output.
+
+This is the transcript so far:
+```md
+{transcript}
+```
+
+Output a json block below with the updated JSON object to be put in the system. Don't yap.
+'''.strip()
+
+ANSWER_PATTERN = r"(?i)ANSWER\s*:\s*(Yes|No)"
 MARKDOWN_PATTERN = r"```md(.*?)```"
+JSON_PATTERN = r"```json(.*?)```"
--- api/test.py
+++ api/test.py
@@ -1,33 +1,227 @@
-from io import BytesIO
+import json
 import os
-from fastapi import FastAPI, File, HTTPException, UploadFile, WebSocket
-from openai import OpenAI
-from pydantic import BaseModel
-from typing import Optional
-from prompts import prompt, prompt_filter, sysprompt, ANSWER_PATTERN, MARKDOWN_PATTERN
-from chatgpt import complete
 import re
-from pydub import AudioSegment
+from openai import OpenAI
+from prompts import JSON_PATTERN, prompt, prompt_extract_drug, sysprompt_checklist, sysprompt_extract_drug, prompt_checklist, sysprompt, ANSWER_PATTERN, MARKDOWN_PATTERN
+from chatgpt import complete
 
 client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
 
-# read in /Users/danielgeorge/Documents/work/ml/small-stuff/doctor-voice-notes/api/1713590705527.webm
 
-file = '/Users/danielgeorge/Documents/work/ml/small-stuff/doctor-voice-notes/api/1713590705527.webm'
+drugs = {
+    "Omeprazole": {
+        "type": "Proton Pump Inhibitor",
+        "contraindications": [
+            {"description": "Must not have severe liver disease", "checked": False},
+            {"description": "Must not be allergic to proton pump inhibitors",
+                "checked": False},
+            {"description": "Must not have low magnesium levels in the blood",
+                "checked": False}
+        ]
+    },
+    "Medical Marijuana": {
+        "contraindications": [
+            {"description": "Must not have heart disease", "checked": False},
+            {"description": "Must not be pregnant", "checked": False},
+            {"description": "Must not have a history of psychosis", "checked": False}
+        ]
+    },
+    "Acetaminophen": {
+        "contraindications": [
+            {"description": "Must not drink three or more alcoholic drinks every day",
+             "checked": False},
+            {"description": "Must not have had liver disease", "checked": False}
+        ]
+    },
+    "Warfarin": {
+        "type": "Blood Thinner",
+        "contraindications": [
+            {"description": "Must not have a bleeding disorder", "checked": False},
+            {"description": "Must not have a history of stroke", "checked": False},
+            {"description": "Must not be pregnant", "checked": False},
+            {"description": "Must not have uncontrolled high blood pressure",
+                "checked": False}
+        ]
+    },
+    "Metformin": {
+        "type": "Antidiabetic Medication",
+        "contraindications": [
+            {"description": "Must not have kidney disease", "checked": False},
+            {"description": "Must not have liver disease", "checked": False},
+            {"description": "Must not be pregnant or planning to become pregnant",
+                "checked": False},
+            {"description": "Must not be allergic to metformin", "checked": False}
+        ]
+    },
+    "Albuterol": {
+        "type": "Bronchodilator",
+        "contraindications": [
+            {"description": "Must not have a history of heart rhythm problems",
+                "checked": False},
+            {"description": "Must not have high blood pressure", "checked": False},
+            {"description": "Must not be allergic to albuterol or related medications",
+             "checked": False},
+            {"description": "Must not be pregnant without consulting a doctor",
+                "checked": False}
+        ]
+    },
+    "Sertraline": {
+        "type": "Selective Serotonin Reuptake Inhibitor - SSRI",
+        "contraindications": [
+            {"description": "Must not have a history of bipolar disorder or mania",
+             "checked": False},
+            {"description": "Must not have a history of seizures", "checked": False},
+            {"description": "Must not be pregnant or breastfeeding without consulting a doctor", "checked": False},
+            {"description":
+             "Must not be taking monoamine oxidase inhibitors (MAOIs) or have taken them within the last 14 days", "checked": False}
+        ]
+    }
+}
 
-# Load the audio file with PyDub
+# Nothing should happen
+chunk1 = """
+Doctor: Hello Mr. George, how are you today?
+Patient: Hello Doctor, I feel a lot more tired than usual and I've been unintentionally losing a lot of weight- I'm a lot skinnier now.
+Doctor: Oh I see, let's get to the bottom of this. Are you experiencing any of the following: frequent urination, blurred vision, or poor wound healing?
+""".strip()
 
-# Load the audio file with PyDub
-# audio = AudioSegment.from_file(BytesIO(audio_data), format="wav")
+# Checklist should appear AND the first item should be checked off
+chunk2 = """
+Doctor: Hello Mr. George, how are you today?
+Patient: Hello Doctor, I feel a lot more tired than usual and I've been unintentionally losing a lot of weight- I'm a lot skinnier now.
+Doctor: Oh I see, let's get to the bottom of this. Are you experiencing any of the following: frequent urination, blurred vision, or poor wound healing?
+Patient: Hm, I think I have been experiencing frequent urination and blurred vision.
+Doctor: It seems like you have Type 2 Diabetes. I am going to prescribe you metformin. Are you on any other medication?
+Patient: No I am not.
+Doctor: Okay. Have you ever had an infection in your liver or kidneys?
+""".strip()
+
+# Checklist should appear AND the second item should be checked off
+# but not the third because that hasn't been said yet
+chunk3 = """
+Doctor: Hello Mr. George, how are you today?
+Patient: Hello Doctor, I feel a lot more tired than usual and I've been unintentionally losing a lot of weight- I'm a lot skinnier now.
+Doctor: Oh I see, let's get to the bottom of this. Are you experiencing any of the following: frequent urination, blurred vision, or poor wound healing?
+Patient: Hm, I think I have been experiencing frequent urination and blurred vision.
+Doctor: It seems like you have Type 2 Diabetes. I am going to prescribe you metformin. Are you on any other medication?
+Patient: No I am not.
+Doctor: Okay. Have you ever had an infection in your liver or kidneys?
+Patient: No, I have not had any. 
+Doctor: Perfect. Are you allergic to any medication?
+""".strip()
+
+# All three has been checked off
+chunk3 = """
+Doctor: Hello Mr. George, how are you today?
+Patient: Hello Doctor, I feel a lot more tired than usual and I've been unintentionally losing a lot of weight- I'm a lot skinnier now.
+Doctor: Oh I see, let's get to the bottom of this. Are you experiencing any of the following: frequent urination, blurred vision, or poor wound healing?
+Patient: Hm, I think I have been experiencing frequent urination and blurred vision.
+Doctor: It seems like you have Type 2 Diabetes. I am going to prescribe you metformin. Are you on any other medication?
+Patient: No I am not.
+Doctor: Okay. Have you ever had an infection in your liver or kidneys?
+Patient: No, I have not had any.
+Doctor: Perfect. Are you allergic to any medication?
+Patient: No.
+""".strip()
+
+list_of_drugs = list(drugs.keys())
 
 
-def transcribe_audio(audio_file_path: str):
-    with open(audio_file_path, 'rb') as audio_file:
-        transcription = client.audio.transcriptions.create(
-            model="whisper-1", file=audio_file)
-    return transcription.text
+# If there isn't a checklist, you need to check for a prescription on the current transcript
+
+checklist = None
+
+# Function to call the complete function and extract the answer
 
 
-# Transcribe the audio
-transcript = transcribe_audio(file)
+def get_checklist(prompt_text, model_version):
+    response = complete(messages=[{"role": "system", "content": sysprompt},
+                                  {"role": "user", "content": prompt_text}], model=model_version)
+    return response
+
+
+# Getting the checklist check
+if not checklist:
+    curr_prompt = prompt_extract_drug.format(transcript=chunk1)
+    response = get_checklist(curr_prompt, "gpt-3.5-turbo")
+    print(response)
+    # Extract the checklist from the response
+    match = re.search(MARKDOWN_PATTERN, response, re.DOTALL)
+    print(checklist)
+    if match:
+        checklist = match.group(1).strip()
+    else:
+        curr_prompt = prompt_extract_drug.format(transcript=chunk1)
+        response = get_checklist(curr_prompt, "gpt-4-turbo-preview")
+        drug = re.search(MARKDOWN_PATTERN, response, re.DOTALL)
+        if match:
+            drug = match.group(1).strip()
+        else:
+            print("Error: Could not extract the checklist from the model's response.")
+            drug = None
+
+# There should be an empty drug
+print('drug should be empty')
+print(f'drug {drug}')
+
+# Getting the checklist check
+if not checklist:
+    curr_prompt = prompt_extract_drug.format(transcript=chunk2)
+    response = get_checklist(curr_prompt, "gpt-3.5-turbo")
+    print(response)
+    # Extract the checklist from the response
+    match = re.search(MARKDOWN_PATTERN, response, re.DOTALL)
+    print(checklist)
+    if match:
+        checklist = match.group(1).strip()
+    else:
+        curr_prompt = prompt_extract_drug.format(transcript=chunk2)
+        response = get_checklist(curr_prompt, "gpt-4-turbo-preview")
+        drug = re.search(MARKDOWN_PATTERN, response, re.DOTALL)
+        if match:
+            drug = match.group(1).strip()
+        else:
+            print("Error: Could not extract the checklist from the model's response.")
+            drug = None
+
+# There should be a drug
+print('drug should be a drug')
+print(f'drug {drug}')
+
+# Getting the checklist from the drug by querying the dict
+if drug:
+    drug = drug.strip()
+    if drug in drugs:
+        checklist = drugs[drug]
+    else:
+        print("Error: Drug not found in the list of drugs.")
+        checklist = None
+
+# Now we can get updates to the checklist based on the transcript
+if checklist:
+    curr_prompt = prompt_checklist.format(
+        drug=drug, checklist=checklist, transcript=chunk2)
+    response = get_checklist(curr_prompt, "gpt-3.5-turbo")
+    print(response)
+    # Extract the updated checklist from the response
+    match = re.search(JSON_PATTERN, response, re.DOTALL)
+    if match:
+        updated_checklist = match.group(1).strip()
+    else:
+        curr_prompt = prompt_checklist.format(
+            drug=drug, checklist=checklist, transcript=chunk2)
+        response = get_checklist(curr_prompt, "gpt-4-turbo-preview")
+        match = re.search(JSON_PATTERN, response, re.DOTALL)
+        if match:
+            updated_checklist = match.group(1).strip()
+        else:
+            print(
+                "Error: Could not extract the updated checklist from the model's response.")
+            updated_checklist = None
+    # Do a json.load on the updated checklist to update the system
+    if updated_checklist:
+        updated_checklist = json.loads(updated_checklist)
+        print(updated_checklist)
+    else:
+        print("Error: Could not update the checklist.")
